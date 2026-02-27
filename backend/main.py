5d35f9106d6d8812bfdf2fdf6d281aef6ff365a9
ayushtiwari1526 <ayushtiwarirs45@gmail.com>
2026-02-27 07:59:04 +0530

    fixed all api urls

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 ﻿from fastapi import FastAPI
 import requests
 from fastapi.middleware.cors import CORSMiddleware
-from backend.utils import get_weather 
+from utils import get_weather 
 import sys
 import os
 
